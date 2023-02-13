c2ece69f3bddf88fb8a34e8d7137c499ee0fe1ec
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-12 23:04:07 -0800

    init

---
+++ main.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Richup Monoply Skin
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes richup.io look like regular Monopoly.
+// @author       Liz Weisman
+// @match        https://richup.io/room/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('test')
+})();
