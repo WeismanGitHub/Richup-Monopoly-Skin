9a78c433624bca3bec5bb32a8742fdb461b627e2
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-02-13 02:30:47 -0800

    created dictionary for the names

--- main.js
+++ main.js
@@ -5,19 +5,49 @@
 // @description  Makes richup.io look like regular Monopoly.
 // @author       Liz Weisman
 // @match        https://richup.io/room/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    document.addEventListener('DOMNodeInserted', (event) => {
-        console.log(event.target.nodeValue)
-        //event.target.replaceChildren("<div>test</div>")
-        // document.getElementsByClassName("wm46CW8f");
-    }, false);
+    const realPropertyNames = {
+        "Salvador": "Mediterranean Avenue",
+        "Rio": "Baltic Avenue",
+        "Tel Aviv": "Oriental Avenue",
+        "Haifa": "Vermont Avenue",
+        "Jerusalem": "Connecticut Avenue",
+        "Venice": "St. Charles Place",
+        "Milan": "States Avenue",
+        "Rome": "Virginia Avenue",
+        "Frankfurt": "St. James Place",
+        "Munich": "Tennessee Avenue",
+        "Berlin": "New York Avenue",
+        "Shenzhen": "Kentucky Avenue",
+        "Beijing": "Indiana",
+        "Shanghai": "Martin Luther King Jr. Boulevard",
+        "Lyon": "Atlantic Avenue",
+        "Toulouse": "Ventnor Avenue",
+        "Paris": "Marvin Gardens",
+        "Liverpool": "Pacific Avenue",
+        "Manchester": "North Carolina Avenue",
+        "London": "Pennsylvania",
+        "San Francisco": "Park Place",
+        "New York": "Boardwalk",
+    }
 
-    /* for (let i = 0; i < propertyNames.length; i++) {
-        propertyNames[i].innerHTML = 'test'
-    } */
+    document.addEventListener('DOMNodeInserted', (event) => {
+        const fakePropertyName = event.target.nodeValue
+        const realPropertyName = realPropertyNames[fakePropertyName]
+
+        if (realPropertyName) {
+            // const realPropertyElement = document.createElement('div')
+            // realPropertyElement.innerHTML = realPropertyName
+
+            // event.target.replaceWith(event.target, realPropertyElement);
+
+            console.log(`${fakePropertyName}: ${realPropertyName}`)
+        }
+
+    }, false);
+    
+    // document.getElementsByClassName("wm46CW8f");
 })();
